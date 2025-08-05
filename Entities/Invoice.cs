e4ba11649c3f8ca30e98011767c9783f88ec4508
Doouglas Silva <paulodnunes2016@gmail.com>
2025-08-05 19:21:09 -0300

    Adiciona estrutura inicial do projeto Xadrez1

---
+++ Entities/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+namespace Xadrez1.Entities
+{
+    internal class Invoice
+    {
+        public double BasiPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basiPayment, double tax)
+        {
+            BasiPayment = basiPayment;
+            Tax = tax;
+        }
+        public double TotalPayment
+        {
+            get { return BasiPayment + Tax }
+        }
+        public override string ToString()
+        {
+            return "Basic payment: "
+                + BasiPayment.ToString("F2",CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2",CultureInfo.InvariantCulture)
+                + "\nTotal payment: "
+                + TotalPayment.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
