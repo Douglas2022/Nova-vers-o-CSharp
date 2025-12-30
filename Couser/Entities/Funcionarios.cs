9748dbcd297a5a35c05b21f5aefc225c90b01d8f
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-30 13:28:59 -0300

    Adicionado Interface incomparable

---
+++ Couser/Entities/Funcionarios.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System;
+
+namespace Couser.Entities
+{
+    class Funcionarios : IComparable
+    {
+        public string Name { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionarios(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0].Trim();
+            Salario = double.Parse(vect[1].Trim(), CultureInfo.InvariantCulture);
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Salario.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Funcionarios))
+            {
+                throw new ArgumentException("Compare o erro: se o argumento não for um funcionário");
+
+            }
+            Funcionarios outro = obj as Funcionarios;
+            return outro.Name.CompareTo(Name);
+        }
+    }
+}
--- Couser/Program.cs
+++ Couser/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Couser.Entities;
 
 namespace Couser
 {
@@ -10,6 +9,29 @@ namespace Couser
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Users\paulo\Downloads\Carla ,4300.00.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionarios> list = new List<Funcionarios>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Funcionarios(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Funcionarios str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
+
