eff07a5649e99476af24797778446ed0c8f97287
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-30 23:32:00 -0300

    Adicionando um Frame

--- 8-tkinter.py
+++ 8-tkinter.py
@@ -1,8 +1,16 @@
 import tkinter as tk
 
-#Criar a nossa janela
+# Criar a nossa janela
 window = tk.Tk()
 window.geometry("300x50")
 window.title("Gerencia frases")
+
+
+# Adicionando um frame
+frame = tk.Frame(window)
+frame.pack(padx=10,pady=10,fill='x',expand=True)
+
+# Adicionando um label
+
 
 window.mainloop()
