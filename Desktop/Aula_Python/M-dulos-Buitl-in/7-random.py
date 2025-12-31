9dda703c61505bc58dec8010c8f1db4a4eb13e21
Doouglas Silva <paulodnunes2016@gmail.com>
2025-12-30 21:56:25 -0300

    Seleciona valores aleatórios de um random

--- Desktop/Aula_Python/M-dulos-Buitl-in/7-random.py
+++ Desktop/Aula_Python/M-dulos-Buitl-in/7-random.py
@@ -6,9 +6,15 @@ import random
 # r1 = random.randint(5,15)
 # print(r1)
 # 1-Seleciona um caractere aleatório de uma syting
-name = 'Eliane'
-r2 = random.choice(name)
-print(r2)
+# name = 'Eliane'
+# r2 = random.choice(name)
+# print(r2)
+# 1-Seleciona mais de um valor aleatório - Rando.sample(sequencia(tamanho)) 
+list1 = [7,6,4,3,2,1]
+list2 = [8,9,7,6,4,3,2,1,0]
+print(random.sample(list1,4))
+print(random.sample(list2,3))
+
 
 
 
