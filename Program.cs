551e06f25fa54ed89c796de29399e81f75f83324
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-04 10:53:04 -0300

    Primeiro commit do projeto Course1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Course1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+            Console.WriteLine("Paulo Douglas");
+            // Teste de alteração para commit
+            // Teste de alteração para commit
+            Console.WriteLine("Alteração");
+
+        }
+
+    }
+
+}
