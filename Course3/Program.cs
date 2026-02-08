0420349c52883a03b232d611d67befa95152f0dd
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-08 12:31:01 -0300

    Conjunto HashSet

---
+++ Course3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+           // Console.WriteLine(set.Contains("Computer")); false
+
+            foreach(string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
