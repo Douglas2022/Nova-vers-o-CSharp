b357d61639a2e77d37e2930d51da2607241bc614
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-08 20:06:51 -0300

    Como o Hash testam a igualdade e exemplo

---
+++ Hash/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        public Product(string name, string price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
