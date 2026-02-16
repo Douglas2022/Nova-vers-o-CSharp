00b248243780f20678a71b7015e768ec924840e7
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-16 09:09:15 -0300

    Exercicios sobre conjuntos

---
+++ Course4/Entities/LogRecods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Course4.Entities
+{
+    internal class LogRecods
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecods))
+            {
+                return false;
+            }
+            LogRecods other = obj as LogRecods;
+            return UserName.Equals(other.UserName);
+        }
+    }
+}
---
+++ Course4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Course4.Entities;
+
+namespace Course4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecods> set = new HashSet<LogRecods>();
+
+            Console.Write("Enter file full path:: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecods { UserName = name, Instant = instant });
+                    }
+                    Console.WriteLine("Total de elementos: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
