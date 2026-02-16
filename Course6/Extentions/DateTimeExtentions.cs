a65634a530054958c680326cc7ba5e6bf92bdeb3
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-16 19:50:17 -0300

    Extension metodos

---
+++ Course6/Extentions/DateTimeExtentions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course6.Extentions
+{
+    static class DateTimeExtentions
+    {
+        public static string ElapsedTime( this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1",CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1",CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ Course6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Course6.Extentions;
+
+namespace Course6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime DT = new DateTime(2018,12,16,8,10,45);
+            Console.WriteLine(DT.ElapsedTime());
+        }
+    }
+}
