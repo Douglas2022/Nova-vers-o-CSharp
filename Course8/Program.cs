44addd66fca1af68a9282bcc2d7fc50e0454eaa2
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-17 20:37:40 -0300

    Atualização com função lambida'Anonima'

--- Course8/Program.cs
+++ Course8/Program.cs
@@ -14,22 +14,17 @@ namespace Course8
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 100.00));
 
-            Comparison<Product> comp = ComparerProducts;
+            Comparison<Product> comp = (p1,p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
             list.Sort(comp);
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
-                Console.WriteLine("Atualizada");
+               
             }
         }
-        static int ComparerProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-        }
-
+   
     }
 
 }
