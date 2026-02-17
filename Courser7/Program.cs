12622122d29a9ef820ba14f361377e559a0e5e8b
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-16 21:01:28 -0300

    Funcao lambida

--- Courser7/Program.cs
+++ Courser7/Program.cs
@@ -15,16 +15,15 @@ namespace Courser7
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            list.Sort(CompareProduct);
+            Comparison<Product> comp = (p1,p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            list.Sort(comp);
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
         }
-        static int CompareProduct(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-        }
+      
     }
 }
