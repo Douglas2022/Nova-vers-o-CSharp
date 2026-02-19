9e42e58baf9d02fd5303523798fbae3b7a4c4c0a
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-19 20:08:44 -0300

    Multiplos delegates

---
+++ Course10/Program.cs
@@ -0,0 +1,25 @@
+﻿using Course10.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course10
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    internal class Program
+
+    {  
+        static void Main(string[] args)
+        {
+            double a = 10; double b = 12;
+
+            BinaryNumericOperation op = CalculationServices.ShowSum;
+            op += CalculationServices.ShowMax;
+
+            op.Invoke(a, b);
+            op(a, b);
+        }
+    }
+}
