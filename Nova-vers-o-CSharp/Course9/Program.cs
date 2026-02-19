a576a2f12dd1a03d1ff7afa853d52d615b2594c6
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-19 19:41:21 -0300

    Outros metodos

--- Nova-vers-o-CSharp/Course9/Program.cs
+++ Nova-vers-o-CSharp/Course9/Program.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace Course9
 {
+    delegate double BinaryNumericOperation(double n1,double n2);
     internal class Program
     {
        
@@ -10,12 +11,24 @@ namespace Course9
         {
             double a = 10; double b = 12;
 
-            double result = CalculateServices.Sum(a, b);
-            double result0 = CalculateServices.Max(a, b);
-            double resulto = CalculateServices.Square(a);
-            Console.WriteLine("Soma é de: " + result);
-            Console.WriteLine("Máximo de 10 é: " + result0);
-            Console.WriteLine("Raiz de (a) é: " + resulto);
+            BinaryNumericOperation op = CalculateServices.Sum;
+            BinaryNumericOperation opi = CalculateServices.Max;
+           
+
+            //double result = CalculateServices.Sum(a, b);
+            //double result0 = CalculateServices.Max(a, b);
+            //double resulto = CalculateServices.Square(a);
+            //Console.WriteLine("Soma é de: " + result);
+            //Console.WriteLine("Máximo de 10 é: " + result0);
+            //Console.WriteLine("Raiz de (a) é: " + resulto);
+
+            double result = op(a,b);
+            Console.WriteLine(result);
+
+            double resulte = opi(a, b);
+            Console.WriteLine(resulte);
+
+          
         }
     }
 }
