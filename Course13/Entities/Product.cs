801a11ffc23d0b44b0bcca02db58b39cf1abc239
Douglas Silva <paulodnunes2016@gmail.com>
2026-02-21 11:03:37 -0300

    Usando um delegate com func

---
+++ Course13/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Course13.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString();
+        }
+    }
+}
---
+++ Course13/Program.cs
@@ -0,0 +1,35 @@
+﻿using Course13.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//Fazer um programa que,a partir de uma lista de produtos,Letras maíscula
+//gere uma nova lista contendo os nomes dos produtos em caixa alta.
+namespace Course13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 900.00));
+            list.Add(new Product("Tablet", 900.00));
+            list.Add(new Product("HD case", 900.00));
+            //Delagate
+            Func<Product, string> func = NameUpper;
+
+            List<string> result = list.Select(func).ToList();
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        static string NameUpper(Product P)
+        {
+            return P.Name.ToUpper();
+        }
+    }
+}
